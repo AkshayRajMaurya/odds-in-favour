5be07f369560730269946df65bbc82e66766d52c
AkshayRajMaurya <akshayrajmaurya@gmail.com>
2016-06-29 23:55:06 +0530

    NewProjectFormat

--- app/src/main/java/com/newapp/easyapp/odds_in_favour/SplashActivity.java
+++ app/src/main/java/com/newapp/easyapp/odds_in_favour/SplashActivity.java
@@ -3,11 +3,11 @@ package com.newapp.easyapp.odds_in_favour;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class Splash extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.splash);
     }
 }
